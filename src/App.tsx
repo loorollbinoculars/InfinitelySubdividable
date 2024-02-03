22262d3dbb4596146219b5a74ba42777e0130505
ducteyes <diego.granero.marana@gmail.com>
2024-02-03 20:53:06 +0000

    Begin work on resizable handles

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ class PanelNode {
 export default function App() {
 	const startGraph = [
 		new PanelNode("1", "row", [
-			// new PanelNode("2", "column", [
-			// 	new PanelNode("3", "row", [new PanelNode("33", "row", [])]),
-			// 	new PanelNode("4", "row", []),
-			// ]),
-			// new PanelNode("5", "column", [new PanelNode("6", "row", [])]),
+			new PanelNode("2", "column", [
+				new PanelNode("3", "row", [new PanelNode("33", "row", [])]),
+				new PanelNode("4", "row", []),
+			]),
+			new PanelNode("5", "column", [new PanelNode("6", "row", [])]),
 		]),
 	];
 	const [graph, setGraph] = useState(startGraph);
@@ -65,7 +65,7 @@ export default function App() {
     */
 		const computedReturn = localGraph.map((el: PanelNode) => (
 			<div
-				className="subdividableElement"
+				className="subdividableWrapper"
 				key={el.id}
 				id={el.id}
 				style={{
@@ -73,6 +73,8 @@ export default function App() {
 					flexBasis: 100 + "%",
 					border: "1px red solid",
 					flexDirection: el.dir,
+					resize: "both",
+					overflow: "auto",
 				}}
 				onClick={(event) => {
 					event.stopPropagation();
@@ -133,9 +135,67 @@ export default function App() {
 		return computedGraph;
 	}
 
+	function getBorders(graph: PanelNode[], elementId: string, side: string) {
+		return null;
+	}
+
 	return (
 		<div className="holder" style={{ resize: "both", overflow: "hidden" }}>
-			{listOfElements}
+			{/* {listOfElements} */}
+			<DiegoResizable />
+		</div>
+	);
+}
+
+function DiegoResizable(props) {
+	const [dragging, setDragging] = useState(false);
+	const [handleBeingDragged, setHandleBeingDragged] = useState(null);
+	const [dims, setDims] = useState([null, null]);
+
+	function clickOnHandle(event, handletype) {
+		setDragging(true);
+		setHandleBeingDragged(handletype);
+	}
+	const emptyImg = new Image();
+	return (
+		<div
+			className="testResizable"
+			style={{
+				width: dims[0] == null ? "100%" : dims[0],
+				height: dims[1] == null ? "100%" : dims[1],
+			}}
+		>
+			<div
+				className="rightHandle"
+				onMouseDown={(e) => {
+					clickOnHandle(e, "right");
+				}}
+				onDragStart={(e) => {
+					e.dataTransfer.setDragImage(emptyImg, 0, 0);
+				}}
+				onDrag={(e) => {
+					console.log(e);
+				}}
+				draggable={true}
+			></div>
+			<div
+				className="bottomHandle"
+				onMouseDown={(e) => {
+					clickOnHandle(e, "bottom");
+				}}
+			></div>
+			<div
+				className="leftHandle"
+				onMouseDown={(e) => {
+					clickOnHandle(e, "left");
+				}}
+			></div>
+			<div
+				className="topHandle"
+				onMouseDown={(e) => {
+					clickOnHandle(e, "top");
+				}}
+			></div>
 		</div>
 	);
 }
