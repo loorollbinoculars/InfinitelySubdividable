3b994bfd53a3a96e8c66676c6102eed9be14d461
ducteyes <diego.granero.marana@gmail.com>
2024-02-05 21:25:20 +0000

    Add binary tree renderer. Recursion finally works. 💩

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@ export default function App() {
 	];
 	const [graph, setGraph] = useState(startGraph);
 	const listOfElements = interpretBinaryGraph(graph);
-	console.log(typeof(listOfElements))
 	function interpretBinaryGraph(localGraph) {
 		/*Graph is a binary tree that can look like:
               o   o
@@ -46,207 +45,137 @@ export default function App() {
       dir: row;
       children:[
         {
-          id:14;
-          dir:column;
-          children:[];  
+			id:14;
+			dir:column;
+			children:[];  
         },
-        {
-          id:15;
-          dir:row;
-          children:[
-            {
-              id:16;
-              dir:row;
-              children:[]
-          }]
-        }
+		{
+			id:16;
+			dir:row;
+			children:[]
+		}]
       ];
     }]
     */
-	return localGraph.map((el) => {
-			if(el.children.length===0){
-				return (<ChildPanel key={Math.random()*10} parent={el} id={el.id} graph={graph} setGraph={setGraph} addChildToGraph={addChildToGraph}></ChildPanel>)
-			}else{
-			return (<ParentPanel key={Math.random()*100} id={el.id} dir={el.dir}>
-				{interpretBinaryGraph(el.children)}
-				</ParentPanel>)
-				
-		}})
+		return localGraph.map((el) => {
+			if (el.children.length === 0) {
+				return (
+					<ChildPanel
+						key={Math.random() * 10}
+						parent={el}
+						id={el.id}
+						graph={graph}
+						setGraph={setGraph}
+						addChildToGraph={addChildToGraph}
+					></ChildPanel>
+				);
+			} else {
+				return (
+					<ParentPanel
+						key={Math.random() * 100}
+						id={el.id}
+						dir={el.dir}
+					>
+						{interpretBinaryGraph(el.children)}
+					</ParentPanel>
+				);
+			}
+		});
+	}
+	function splitChildNode(node, newElement, newDir) {
+		return new PanelNode(
+			node.id + "parent",
+			newDir,
+			[node, newElement],
+			""
+		);
 	}
 
 	function addChildToGraph(
 		graph: PanelNode[],
 		id: string,
 		newElement: PanelNode,
-		newDir: string,
-		previousElement={}
-	) {
-		console.log(previousElement)
-		const computedGraph = graph.map((element: PanelNode) => {
-			if (element.id == id) {
-				previousElement.children.push(new PanelNode("Constructed", element.dir, [element, newElement]))
-				const idx = previousElement.children.indexOf(element)
-				previousElement.children.splice(idx, idx+1)
-			} else {
-				element.children = addChildToGraph(
-					element.children,
+		newDir: string
+	): PanelNode[] {
+		return graph.map((node) => {
+			if (node.id === id) {
+				return splitChildNode(node, newElement, newDir);
+			}
+
+			if (node.children.length === 0) {
+				return node;
+			}
+
+			// Recursively search for the node in the children
+			return {
+				...node,
+				children: addChildToGraph(
+					node.children,
 					id,
 					newElement,
-					newDir,
-					element
-				);
-			}
-			return element;
+					newDir
+				),
+			};
 		});
-		return computedGraph;
 	}
-
-	return (
-		<Holder key={1}>{listOfElements}
-		</Holder>
-	);
+	return <Holder key={1}>{listOfElements}</Holder>;
 }
 
-function Holder(props){
-  return <div className="holder">{props.children}</div>
+function Holder(props) {
+	return <div className="holder">{props.children}</div>;
 }
 
-function DiegoResizable(props) {
-	const [dragging, setDragging] = useState(false);
-	const [handleBeingDragged, setHandleBeingDragged] = useState(null);
-	const [dim, setDim] = useState(80);
-	function clickOnHandle(event, handletype) {
-		setDragging(true);
-		setHandleBeingDragged(handletype);
-	}
-
-	const emptyImg = new Image();
+function ParentPanel(props) {
 	return (
 		<div
-			className="testResizable"
-			style={{
-				flexBasis: dim + "%",
-			}}
+			className="ParentPanel"
+			id={props.id}
+			style={{ flexDirection: props.dir }}
 		>
-			<div
-				className="rightHandle"
-				onPointerDown={(e) => {
-					clickOnHandle(e, "right");
-				}}
-				onPointerMove={(event) => {
-					if (dragging) {
-						// console.log(event.clientX + "  " + event.clientY);
-						// console.log(event.target.getBoundingClientRect());
-					}
-					// setDim((previous) => previous - 1);
-				}}
-				onPointerUp={() => setDragging(false)}
-				onDragEnd={() => console.log("ENDED")}
-				draggable={true}
-			></div>
-			<div
-				className="bottomHandle"
-				onMouseDown={(e) => {
-					clickOnHandle(e, "bottom");
-				}}
-			></div>
-			<div
-				className="leftHandle"
-				onMouseDown={(e) => {
-					clickOnHandle(e, "left");
-				}}
-			></div>
-			<div
-				className="topHandle"
-				onMouseDown={(e) => {
-					clickOnHandle(e, "top");
-				}}
-			></div>
+			{props.children}
 		</div>
 	);
 }
 
-function interpretGraph(graph, c) {
-	const colors = [
-		"green",
-		"red",
-		"blue",
-		"gray",
-		"white",
-		"yellow",
-		"brown",
-		"aliceblue",
-		"darkgreen",
-	];
-	if (graph.length == 1) {
-		return (
-			<div
-				className="subdividableElement"
-				style={{
-					display: "flex",
-					flexBasis: 50 + "%",
-					backgroundColor: colors[c],
-				}}
-			></div>
-		);
-	} else {
-		return (
-			<div
-				className="subdividableElement"
-				style={{
-					display: "flex",
-					flexBasis: 50 + "%",
-					backgroundColor: colors[c],
-					flexDirection: graph[0].dir,
-				}}
-			>
-				{interpretGraph(graph.slice(1), c + 1)}
-			</div>
-		);
-	}
-}
-
-function ParentPanel(props){
-  return <div className="ParentPanel" id = {props.id} style={{flexDirection: props.dir}}>{props.children}</div>
-}
-
-function ChildPanel(props){
-  return <div
-    className="subdividableWrapper"
-	id={props.id}
-    style={{
-      display: "flex",
-      flexBasis: 100 + "%",
-      border: "1px red solid"
-    }}
-    onClick={(event) => {
-      event.stopPropagation();
-      // console.log(event.target.id);
-      const rect = event.target.getBoundingClientRect();
-      const xProportion =
-        (event.clientX - rect.left) / rect.width;
-      const yProportion =
-        (event.clientY - rect.top) / rect.height;
-      let dir = "row";
-      if (xProportion >= 0.8 || xProportion <= 0.2) {
-        dir = "row";
-      }
-      if (yProportion >= 0.8 || yProportion <= 0.2) {
-        dir = "column";
-      }
-      props.setGraph(() =>
-        props.addChildToGraph(
-          props.graph,
-          props.parent.id,
-          new PanelNode(
-            props.parent.id + String(Math.random() * 100),
-            dir,
-            []
-          ),
-          dir
-        )
-      );
-    }}
-    >{props.children ? props.children : null}
-  </div>
+function ChildPanel(props) {
+	return (
+		<div
+			className="subdividableWrapper"
+			id={props.id}
+			style={{
+				display: "flex",
+				flexBasis: 100 + "%",
+				border: "1px red solid",
+			}}
+			onClick={(event) => {
+				event.stopPropagation();
+				const rect = event.target.getBoundingClientRect();
+				const xProportion = (event.clientX - rect.left) / rect.width;
+				const yProportion = (event.clientY - rect.top) / rect.height;
+				let dir = "row";
+				if (xProportion >= 0.8 || xProportion <= 0.2) {
+					dir = "row";
+				}
+				if (yProportion >= 0.8 || yProportion <= 0.2) {
+					dir = "column";
+				}
+				props.setGraph((previous) =>
+					props.addChildToGraph(
+						props.graph,
+						props.parent.id,
+						new PanelNode(
+							props.parent.id +
+								String(Math.random() * 100) +
+								"child",
+							dir,
+							[]
+						),
+						dir
+					)
+				);
+			}}
+		>
+			{props.children ? props.children : null}
+		</div>
+	);
 }
