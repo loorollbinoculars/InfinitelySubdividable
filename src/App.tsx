0e2c11826dc5e58a35c05a34e79cf8bc60f041f1
diego.marana <diego.marana@cmrsurgical.com>
2024-02-06 11:20:05 +0000

    Add unique ids to each element, while preserving ids of existing elements in addChildToGraph()

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import { useEffect, useState, useRef } from "react";
-import { DndContext } from "@dnd-kit/core";
-import { Subdividable } from "./Subdividable";
-import { Resizable } from "./Resizable";
-import { Droppable } from "./Droppable";
-import { Draggable } from "./Draggable";
+import { v4 as uuidv4 } from "uuid";
 
 class PanelNode {
-	id: string;
-	dir: string;
-	children: PanelNode[];
-	parent: string;
+  id: string;
+  dir: string;
+  children: PanelNode[];
+  parent: string;
 
-	constructor(id: string, dir: string, children: PanelNode[], parent = "") {
-		this.id = id;
-		this.dir = dir;
-		this.children = children;
-		this.parent = parent;
-	}
+  constructor(id: string, dir: string, children: PanelNode[], parent = "") {
+    this.id = id || uuidv4();
+    this.dir = dir;
+    this.children = children;
+    this.parent = parent;
+  }
 }
 
 export default function App() {
-	const startGraph = [
-		new PanelNode("1", "row", [
-			new PanelNode("2", "column", [
-				new PanelNode("3", "row", [new PanelNode("33", "row", [])]),
-				new PanelNode("4", "row", []),
-			]),
-			new PanelNode("5", "column", [new PanelNode("6", "row", [])]),
-		]),
-	];
-	const [graph, setGraph] = useState(startGraph);
-	const listOfElements = interpretBinaryGraph(graph);
-	function interpretBinaryGraph(localGraph) {
-		/*Graph is a binary tree that can look like:
+  const startGraph = [
+    new PanelNode("1", "row", [
+      new PanelNode(uuidv4(), "column", [
+        new PanelNode("3", "row", [new PanelNode("33", "row", [])]),
+        new PanelNode("4", "row", []),
+      ]),
+      new PanelNode("5", "column", [new PanelNode("6", "row", [])]),
+    ]),
+  ];
+  const [graph, setGraph] = useState(startGraph);
+  const listOfElements = interpretBinaryGraph(graph);
+  function interpretBinaryGraph(localGraph) {
+    /*Graph is a binary tree that can look like:
               o   o
             /    /
     o--o--o-----o
@@ -57,147 +53,142 @@ export default function App() {
       ];
     }]
     */
-		return localGraph.map((el) => {
-			if (el.children.length === 0) {
-				return (
-					<ChildPanel
-						key={Math.random() * 10}
-						parent={el}
-						id={el.id}
-						graph={graph}
-						setGraph={setGraph}
-						addChildToGraph={addChildToGraph}
-					></ChildPanel>
-				);
-			} else {
-				return (
-					<ParentPanel
-						key={Math.random() * 100}
-						id={el.id}
-						dir={el.dir}
-					>
-						{interpretBinaryGraph(el.children)}
-					</ParentPanel>
-				);
-			}
-		});
-	}
-	function splitChildNode(node, newElement, newDir) {
-		return new PanelNode(
-			node.id + "parent",
-			newDir,
-			[node, newElement],
-			""
-		);
-	}
+    const stringToColour = (str: string) => {
+      let hash = 0;
+      str.split("").forEach((char) => {
+        hash = char.charCodeAt(0) + ((hash << 5) - hash);
+      });
+      let colour = "#";
+      for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff;
+        colour += value.toString(16).padStart(2, "0");
+      }
+      return colour;
+    };
 
-	function addChildToGraph(
-		graph: PanelNode[],
-		id: string,
-		newElement: PanelNode,
-		newDir: string
-	): PanelNode[] {
-		return graph.map((node) => {
-			if (node.id === id) {
-				return splitChildNode(node, newElement, newDir);
-			}
+    return localGraph.map((el) => {
+      if (el.children.length === 0) {
+        return (
+          <ChildPanel
+            key={el.id} 
+            parent={el}
+            id={el.id}
+            graph={graph}
+            setGraph={setGraph}
+            addChildToGraph={addChildToGraph}
+            color={stringToColour(el.id)}></ChildPanel>
+        );
+      } else {
+        return (
+          <ParentPanel key={el.id} id={el.id} dir={el.dir}>
+            {interpretBinaryGraph(el.children)}
+          </ParentPanel>
+        );
+      }
+    });
+  }
+  function splitChildNode(node, newElement, newDir) {
+    return new PanelNode(uuidv4(), newDir, [node, newElement], "");
+  }
 
-			if (node.children.length === 0) {
-				return node;
-			}
+  function addChildToGraph(
+    graph: PanelNode[],
+    id: string,
+    newElement: PanelNode,
+    newDir: string
+  ): PanelNode[] {
+    return graph.map((node) => {
+      if (node.id === id) {
+        return splitChildNode(node, newElement, newDir);
+      }
 
-			// Recursively search for the node in the children
-			return {
-				...node,
-				children: addChildToGraph(
-					node.children,
-					id,
-					newElement,
-					newDir
-				),
-			};
-		});
-	}
-	return <Holder key={1}>{listOfElements}</Holder>;
+      if (node.children.length === 0) {
+        return node;
+      }
+
+      // Recursively search for the node in the children
+      return {
+        ...node,
+        children: addChildToGraph(node.children, id, newElement, newDir),
+      };
+    });
+  }
+  return <Holder key={1}>{listOfElements}</Holder>;
 }
 
 function Holder(props) {
-	return <div className="holder">{props.children}</div>;
+  return <div className="holder">{props.children}</div>;
 }
 
 function ParentPanel(props) {
-	return (
-		<div
-			className="ParentPanel"
-			id={props.id}
-			style={{ flexDirection: props.dir }}
-			onPointerDown={(event)=>{
-				event.stopPropagation();
-				const rect = event.target.getBoundingClientRect();
-				const xProportion = (event.clientX - rect.left) / rect.width;
-				console.log(props.children)
-			}}
-		>
-			{props.children}
-		</div>
-	);
+  const [dragging, setDragging] = useState(false);
+  return (
+    <div
+      className="ParentPanel"
+      id={props.id}
+      style={{ flexDirection: props.dir }}
+      onPointerDown={(event) => {
+        event.stopPropagation();
+        const rect = event.target.getBoundingClientRect();
+        const xProportion = (event.clientX - rect.left) / rect.width;
+        console.log(props.children);
+      }}
+    >
+      {props.children}
+    </div>
+  );
 }
 
 function ChildPanel(props) {
-	const [basis, setBasis] = useState(100);
-	const [screenXOriginal, setScreenXOriginal] = useState(0);
-	const [screenYOriginal, setScreenYOriginal] = useState(0);
-	const [dragging, setDragging] = useState(false);
-	return (
-		<div
-			className="subdividableWrapper"
-			id={props.id}
-			style={{
-				display: "flex",
-				flexBasis: basis + "%",
-				border: "1px red solid",
-				alignItems: "center",
-				justifyContent: "center",
-			}}
-			onPointerDown={(event) => {
-				setScreenXOriginal(event.screenX);
-				setScreenYOriginal(event.screenY);
-				setDragging(() => true);
-				const rect = event.target.getBoundingClientRect();
-				const xProportion = (event.clientX - rect.left) / rect.width;
-				const yProportion = (event.clientY - rect.top) / rect.height;
-				let dir = "row";
-				if (xProportion >= 0.8 || xProportion <= 0.2) {
-					dir = "row";
-				}
-				if (yProportion >= 0.8 || yProportion <= 0.2) {
-					dir = "column";
-				}
-				props.setGraph((previous) =>
-					props.addChildToGraph(
-						props.graph,
-						props.parent.id,
-						new PanelNode(
-							props.parent.id +
-								String(Math.random() * 100) +
-								"child",
-							dir,
-							[]
-						),
-						dir
-					)
-				);
-			}}
-			onPointerMove={(event) => {
-				if (dragging) {
-					setBasis((previous) => previous - 1);
-				}
-			}}
-			// onMouseLeave={(event) => {
-			// 	setDragging(false);
-			// }}
-		>
-			{props.children}
-		</div>
-	);
+  const [basis, setBasis] = useState(100);
+  const [screenXOriginal, setScreenXOriginal] = useState(0);
+  const [screenYOriginal, setScreenYOriginal] = useState(0);
+  const [dragging, setDragging] = useState(false);
+  return (
+    <div
+      className="subdividableWrapper"
+      id={props.id}
+      style={{
+        display: "flex",
+        flexBasis: basis + "%",
+        border: "1px red solid",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: props.color,
+      }}
+      onPointerDown={(event) => {
+        setScreenXOriginal(event.screenX);
+        setScreenYOriginal(event.screenY);
+        setDragging(() => true);
+        const rect = event.target.getBoundingClientRect();
+        const xProportion = (event.clientX - rect.left) / rect.width;
+        const yProportion = (event.clientY - rect.top) / rect.height;
+        let dir = "row";
+        if (xProportion >= 0.8 || xProportion <= 0.2) {
+          dir = "row";
+        }
+        if (yProportion >= 0.8 || yProportion <= 0.2) {
+          dir = "column";
+        }
+        props.setGraph((previous) =>
+          props.addChildToGraph(
+            props.graph,
+            props.parent.id,
+            new PanelNode(uuidv4(), dir, []),
+            dir
+          )
+        );
+      }}
+      onPointerMove={(event) => {
+        if (dragging) {
+          setBasis((previous) => previous - 1);
+        }
+      }}
+      // onMouseLeave={(event) => {
+      // 	setDragging(false);
+      // }}
+    >
+      {props.children}
+    </div>
+  );
 }
